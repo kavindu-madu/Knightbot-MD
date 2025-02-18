237cfe49ee668b76f70851b8655df3fb605fa83e
kavindu-madu <142071735+kavindu-madu@users.noreply.github.com>
2025-02-19 02:38:41 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: 'Devil',
+  author: 'Dev Madu',
+  botName: "Devil Bot",
+  botOwner: 'Devil Madu', // Your name
+  ownerNumber: '94772411839', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
